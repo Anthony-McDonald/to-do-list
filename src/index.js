779fba9a5f98db104b2d1fa312f038b7cbd2b3bc
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2023-11-09 16:54:04 +0000

    webpack template for future projects

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import './style.css'
+// import Icon from './icon.png';
+// import Data from './data.xml';
+// import Notes from './data.csv';
+// import yaml from './data.yaml'
+
+// console.log(yaml.title);    for example
+
+
+ function component() {
+   const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//    element.classList.add("test")
+
+
+//   // Add the image to our existing div.
+
+//   const myIcon = new Image();
+
+//   myIcon.src = Icon;
+
+
+//   element.appendChild(myIcon);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const yaml = require('yamljs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+  mode: 'development',
+   entry: './src/index.js',
+   devServer: {
+    static: './dist',
+   },
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+
+   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack template',
+    }),
+   ],
+   module: {
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource',
+       },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+       },
+       {
+         test: /\.(csv|tsv)$/i,
+         use: ['csv-loader'],
+       },
+       {
+         test: /\.xml$/i,
+         use: ['xml-loader'],
+       },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      
+     ],
+   },
+ };
