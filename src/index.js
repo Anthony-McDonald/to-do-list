6ec57a252b8e1741b1d88b36f8d4c0a2b80aa7a3
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2023-11-10 01:35:31 +0000

    base page gui and svgs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css'
-// import Icon from './icon.png';
+import journalIcon from './assets/notebook.svg';
+import moreIcon from './assets/more.svg'
 // import Data from './data.xml';
 // import Notes from './data.csv';
 // import yaml from './data.yaml'
@@ -9,10 +10,41 @@ import './style.css'
 
 
  function component() {
+  // Define wrapper div
    const element = document.createElement('div');
+   element.classList.add("wrapper");
+  // Header elements
+  // Header left
+    const header = document.createElement('div');
+    header.classList.add("header");
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const headerLeft = document.createElement('div');
+    headerLeft.classList.add('headerleft')
+
+    const headerText = document.createElement('h3')
+    headerText.classList.add('headertext');
+    headerText.innerText = "// TO-DO List //"
+
+    const noteSVG = new Image();
+    noteSVG.src = journalIcon;
+    noteSVG.classList.add('svg');
+  //Header right
+    const dropdown = document.createElement('button');
+    const moreSVG = new Image();
+    moreSVG.src = moreIcon;
+    moreSVG.classList.add('svg');
+
+    dropdown.appendChild(moreSVG);
+
+
+    headerLeft.appendChild(headerText);
+    headerLeft.appendChild(noteSVG);
+
+    header.appendChild(headerLeft);
+    header.appendChild(dropdown);
+    // Main elements
+    const main = document.createElement('div');
+    main.classList.add("main");
 //    element.classList.add("test")
 
 
@@ -25,7 +57,12 @@ import './style.css'
 
 //   element.appendChild(myIcon);
 
+element.appendChild(header);
+element.appendChild(main);
+
    return element;
  }
 
  document.body.appendChild(component());
+
+
