63b7862435f32a4524b6dfc72a5eefe40feb107c
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-01-13 17:24:24 +0000

    tasks in home can now be added and deleted and will maintain permenance between sessions due to localhost storage

--- src/home.js
+++ src/home.js
@@ -22,18 +22,30 @@ export default function homeMain() {
 
     const contentDiv = document.createElement('div');
 
-    const exampleToDo = new todoItem(1, 'brush teeth', 'with toothpaste', '15/06/2024', 'medium', null, true,);
-    const exampleToDo2 = new todoItem(2, 'brush teeth', 'with toothpaste', '15/06/2024', 'high', null, false,);
-    const exampleToDo3 = new todoItem('brush teeth', 'with toothpaste', '15/06/2024', 'high', null, false,);
-    const exampleToDo4 = new todoItem('brush teeth', 'with toothpaste', '15/06/2024', 'high', null, false,);
+    // const exampleToDo = new todoItem(1, 'brush teeth', 'with toothpaste', '15/06/2024', 'medium', null, true,);
+    // const exampleToDo2 = new todoItem(2, 'brush teeth', 'with toothpaste', '15/06/2024', 'high', null, false,);
+    // const exampleToDo3 = new todoItem('brush teeth', 'with toothpaste', '15/06/2024', 'high', null, false,);
+    // const exampleToDo4 = new todoItem('brush teeth', 'with toothpaste', '15/06/2024', 'high', null, false,);
     addNewToDo.addEventListener('click', function() {
         homeMain.appendChild(showTaskForm(contentDiv));
         
         
     });
 
-    contentDiv.appendChild(exampleToDo.returnDiv());
-    contentDiv.appendChild(exampleToDo2.returnDiv());
+    // contentDiv.appendChild(exampleToDo.returnDiv());
+    // contentDiv.appendChild(exampleToDo2.returnDiv());
+    let idsInStorage = localStorage.getItem("tItemID");
+    for (let i = -1; i < parseInt(idsInStorage); i++) {
+        console.log("entry")
+        
+        let jsonObject = JSON.parse(localStorage.getItem(i + 1))
+        if (jsonObject != null) {
+            console.log(jsonObject);
+            let nextEntry = new todoItem(jsonObject.itemNumber, jsonObject.title, jsonObject.description, jsonObject.dueDate,jsonObject.priority,jsonObject.attachedNotes);
+            contentDiv.appendChild(nextEntry.returnDiv());
+        }
+
+    };
 
 
     titleDiv.appendChild(title);
